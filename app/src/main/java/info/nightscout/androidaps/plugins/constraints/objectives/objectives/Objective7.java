8d038d5e18b946754b41162ec26259e90dced2e6
iain <iain.ct@gmail.com>
2020-01-11 21:30:00 +1030

    Changing Objective Time from Days to Mins
    Change the SMBDefaults

--- app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/objectives/Objective7.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/objectives/Objective7.java
@@ -14,6 +14,6 @@ public class Objective7 extends Objective {
 
     @Override
     protected void setupTasks(List<Task> tasks) {
-        tasks.add(new MinimumDurationTask(T.mins(5).msecs()));
+        tasks.add(new MinimumDurationTask(T.mins(1).msecs()));
     }
 }
